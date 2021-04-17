e819e2f230a9023ddc1a3b5b202387beadec19bb
hexu <hexu_1985@sina.com>
2021-04-18 00:13:45 +0800

    complete chapter-04

--- chapter-03/ch3_39.py
+++ chapter-03/ch3_39.py
@@ -1,10 +1,9 @@
 from tkinter import * 
 root = Tk()
-root.title("ch3_38")
-# root.geometry("1280x800")
-root.geometry("128x800")
+root.title("ch3_39")
+root.geometry("640x480")
 
-night = PhotoImage(file="1_cropped.png")
+night = PhotoImage(file="snow.png")
 label = Label(root,image=night)
 label.place(relx=0.1,rely=0.1,relheight=0.8)
 
